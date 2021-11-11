1214829d18679a1abdc3ab32cd20abd7d005aa4f
faysaljafry <faysaljafryofficial@gmail.com>
2021-11-12 02:46:05 +0500

    new Build

---
+++ dist/precache-manifest.78ce0b2db48421b299d52ef323da225b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b302ab0ce8c46dee5360",
+    "url": "css/app.93a42e67.css"
+  },
+  {
+    "revision": "9c0fd5f944cc7d8087417f6560097025",
+    "url": "index.html"
+  },
+  {
+    "revision": "b302ab0ce8c46dee5360",
+    "url": "js/app.20485f0e.js"
+  },
+  {
+    "revision": "b4bb9d1221e7fd31289c",
+    "url": "js/chunk-vendors.23fbd908.js"
+  },
+  {
+    "revision": "5236286ea72ed65dbec3b6a0af9834c5",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);

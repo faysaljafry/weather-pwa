402744329530e89c10fb904a37ebb2b3ba306f44
faysaljafry <faysaljafryofficial@gmail.com>
2021-11-12 20:58:47 +0500

    an other version

---
+++ dist/precache-manifest.8b8d246b23d1e522f6ffe1e2a266514b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94faa40aff7a2a6c4dae",
+    "url": "css/app.2ba98996.css"
+  },
+  {
+    "revision": "feb717bb999fa8d1ac1a",
+    "url": "css/chunk-vendors.da387543.css"
+  },
+  {
+    "revision": "b4451c501a5220911228b127d56449bb",
+    "url": "index.html"
+  },
+  {
+    "revision": "94faa40aff7a2a6c4dae",
+    "url": "js/app.6d5aaf48.js"
+  },
+  {
+    "revision": "feb717bb999fa8d1ac1a",
+    "url": "js/chunk-vendors.05fbfb01.js"
+  },
+  {
+    "revision": "5236286ea72ed65dbec3b6a0af9834c5",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
